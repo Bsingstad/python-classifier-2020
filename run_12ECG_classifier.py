ba356a0643ee16979aa18b9fd56621f6e732476c
Bjørn-Jostein Singstad <bjorn_sing@hotmail.com>
2020-04-05 14:12:45 +0200

    Add the model for this branch
    Add the model for this branch

--- run_12ECG_classifier.py
+++ run_12ECG_classifier.py
@@ -3,6 +3,14 @@
 import numpy as np
 import joblib
 from get_12ECG_features import get_12ECG_features
+<<<<<<< Updated upstream
+=======
+from keras.models import Sequential,Model,load_model
+from keras.optimizers import Adam
+from scipy.io import loadmat
+#import numpy as np, os, sys
+from keras.layers import Dense, LSTM, Dropout, TimeDistributed, Bidirectional, InputLayer, Flatten
+>>>>>>> Stashed changes
 
 def run_12ECG_classifier(data,header_data,classes,model):
 
@@ -16,6 +24,7 @@ def run_12ECG_classifier(data,header_data,classes,model):
     label = model.predict(feats_reshape)
     score = model.predict_proba(feats_reshape)
 
+<<<<<<< Updated upstream
     current_label[label] = 1
 
     for i in range(num_classes):
@@ -27,5 +36,33 @@ def load_12ECG_model():
     # load the model from disk 
     filename='finalized_model.sav'
     loaded_model = joblib.load(filename)
+=======
+def load_12ECG_model():
+    # load the model from disk
+   # model = Sequential()
+    #model.add(LSTM(32, input_shape=(1, 12)))
+    #model.add(Dense(32, activation='relu'))
+    #model.add(Dropout(0.5))
+    #model.add(Dense(9, activation='softmax'))
+
+    model = Sequential()
+    model.add(InputLayer(input_shape=(1, 12)))
+    model.add(Bidirectional(LSTM(100, activation='tanh', return_sequences=True)))
+    model.add(Dropout(0.2))
+    model.add(LSTM(100,activation='tanh', return_sequences=True))
+    model.add(Dropout(0.2))
+    model.add(TimeDistributed(Dense(500)))
+    model.add(Dropout(0.2))
+    model.add(Dense(500, activation='relu'))
+    model.add(Dense(9, activation='sigmoid'))
+    model.add(Flatten())
+    model.add(Dense(9, activation='sigmoid'))
+
+    #model.load_weights("LSTM_physionet_comp2020.h5")
+    model.load_weights("bidir_LSTM_physionet_comp2020.h5")
+
+    #model.compile(loss='categorical_crossentropy', optimizer="Adam", metrics=['acc'])
+    model.compile(loss='categorical_crossentropy', optimizer="SGD", metrics=['categorical_accuracy', 'categorical_crossentropy'])
+>>>>>>> Stashed changes
 
     return loaded_model
