e16e6400fb77986d4c205aed148657961167bd52
Bjørn-Jostein Singstad <bjorn_sing@hotmail.com>
2020-04-23 20:36:31 +0200

    New model
    add new model

--- run_12ECG_classifier.py
+++ run_12ECG_classifier.py
@@ -77,7 +77,7 @@ def load_12ECG_model():
     #model.add(Dense(9, activation='softmax'))
 
 
-    model.load_weights("weights_best.hdf5")
+    model.load_weights("weights_512_22.april.hdf5")
 
     model.compile(loss='categorical_crossentropy', optimizer="SGD", metrics=['categorical_accuracy', 'categorical_crossentropy'])
 
